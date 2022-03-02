93e36def0f2797691d0ce8d4e7a8e5c95e671c10
Saulo de Társio <saulodetarsio107@gmail.com>
2022-03-02 18:28:23 -0300

    Ajustando encoding da documentação.

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"questoes"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"questoes"},{"l":"testes"}];updateSearchResults();
--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"questoes","l":"Questao1"},{"p":"questoes","l":"Questao2"},{"p":"questoes","l":"Questao3"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"questoes","l":"Questao1"},{"p":"questoes","l":"Questao2"},{"p":"questoes","l":"Questao3"},{"p":"testes","l":"Tests"}];updateSearchResults();
